b23bceaecfb0008c68e473f9a184dc6e84f38a54
fayshal-bin-amir <foyshalbinamir@gmail.com>
2024-02-13 23:09:52 +0600

    make this half responsive

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ function play() {
     //1. hide the home screen by adding 'hidden' class on home page
     const homeSection = document.getElementById('home');
     homeSection.classList.add('hidden');
-    homeSection.classList.remove('flex');
 
     // 2. show the play ground by removing the 'hidden' class on play ground
     const playGround = document.getElementById('play-ground');
